288e13601ef8c44451a05503c613a634743e1ff3
DeployChef <dc.titovvlad@gmail.com>
2020-04-03 13:05:07 +0300

    Добавил проект для тестов

---
+++ Backend/SuggestService/Controllers/SuggestController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Logging.Abstractions;
+
+namespace SuggestService.Controllers
+{
+    [ApiController]
+    [Route("api/suggest")]
+    public class SuggestController : Controller
+    {
+        private readonly ILogger<SuggestController> _logger;
+
+        public SuggestController(ILogger<SuggestController> logger)
+        {
+            _logger = logger ?? new NullLogger<SuggestController>();
+        }
+    }
+}
