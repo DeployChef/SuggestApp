b3fb5f0113a885fea357ef8035e2d45d3a64d55d
DeployChef <dc.titovvlad@gmail.com>
2020-04-05 15:25:20 +0300

    закоментил тесты на репу

--- Backend/SuggestService.Tests/UnitTests/SuggestRepositoryTests/WhenSuggestRepositoryGetSuggests.cs
+++ Backend/SuggestService.Tests/UnitTests/SuggestRepositoryTests/WhenSuggestRepositoryGetSuggests.cs
@@ -9,28 +9,28 @@ namespace SuggestService.Tests.UnitTests.SuggestRepositoryTests
 {
     public class WhenSuggestRepositoryGetSuggests
     {
-        [Theory]
-        [MemberData(nameof(SuggestParameterGenerator.GetSuggests), MemberType = typeof(SuggestParameterGenerator))]
-        public async Task GetSuggest_ReturnOk(string[] suggests)
-        {
+        //[Theory]
+        //[MemberData(nameof(SuggestParameterGenerator.GetSuggests), MemberType = typeof(SuggestParameterGenerator))]
+        //public async Task GetSuggest_ReturnOk(string[] suggests)
+        //{
 
-            //КОГДА НИБУДЬ OrmLite ЗАРАБОТАЕТ ДЛЯ ПОСТГРЕСА В ИНМЕМОРИ, ТОГДА И ВСЕ ЗАРАБОТАЕТ :))))
+        //    //КОГДА НИБУДЬ OrmLite ЗАРАБОТАЕТ ДЛЯ ПОСТГРЕСА В ИНМЕМОРИ, ТОГДА И ВСЕ ЗАРАБОТАЕТ :))))
 
 
-            //var suggest = suggests.Select(c => new SuggestEntity() {Suggestion = c}).ToList();
-            //var db = new InMemoryDatabase();
-            //db.Insert(suggest);
+        //    //var suggest = suggests.Select(c => new SuggestEntity() {Suggestion = c}).ToList();
+        //    //var db = new InMemoryDatabase();
+        //    //db.Insert(suggest);
 
-            //var connectionFactory = new Mock<IDbConnectionFactory>();
-            //connectionFactory.Setup(c => c.Create()).Returns(() => db.OpenConnection());
+        //    //var connectionFactory = new Mock<IDbConnectionFactory>();
+        //    //connectionFactory.Setup(c => c.Create()).Returns(() => db.OpenConnection());
 
-            //var repository = new SuggestRepository(connectionFactory.Object);
+        //    //var repository = new SuggestRepository(connectionFactory.Object);
 
-            //var result = await repository.GetSuggestsAsync("input", CancellationToken.None);
+        //    //var result = await repository.GetSuggestsAsync("input", CancellationToken.None);
 
-            //result.Count.Should().BeLessOrEqualTo(10);
-            //result.Select(c=>c.Suggestion).Should().BeEquivalentTo(suggests.Take(10));
-        }
+        //    //result.Count.Should().BeLessOrEqualTo(10);
+        //    //result.Select(c=>c.Suggestion).Should().BeEquivalentTo(suggests.Take(10));
+        //}
 
 
         //КОГДА НИБУДЬ OrmLite ЗАРАБОТАЕТ ДЛЯ ПОСТГРЕСА В ИНМЕМОРИ, ТОГДА И ВСЕ ЗАРАБОТАЕТ :))))
